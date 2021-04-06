e7b52e927dce5284239ebedf88193de9449dce20
alailsonko <aalailson3@gmail.com>
2021-04-06 09:22:23 -0300

    chore: start project

---
+++ blog_api/test/support/channel_case.ex
@@ -0,0 +1,40 @@
+defmodule BlogApiWeb.ChannelCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  channel tests.
+
+  Such tests rely on `Phoenix.ChannelTest` and also
+  import other functionality to make it easier
+  to build common data structures and query the data layer.
+
+  Finally, if the test case interacts with the database,
+  we enable the SQL sandbox, so changes done to the database
+  are reverted at the end of every test. If you are using
+  PostgreSQL, you can even run database tests asynchronously
+  by setting `use BlogApiWeb.ChannelCase, async: true`, although
+  this option is not recommended for other databases.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      # Import conveniences for testing with channels
+      import Phoenix.ChannelTest
+      import BlogApiWeb.ChannelCase
+
+      # The default endpoint for testing
+      @endpoint BlogApiWeb.Endpoint
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(BlogApi.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(BlogApi.Repo, {:shared, self()})
+    end
+
+    :ok
+  end
+end
