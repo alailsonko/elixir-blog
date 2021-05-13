15cf74ebdc5e581ee5adf83e3c972152d339219b
alailsonko <aalailson3@gmail.com>
2021-05-13 08:02:21 -0300

    start learning elixir

--- blog_api/lib/blog_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule BlogApi.Repo do
-  use Ecto.Repo,
-    otp_app: :blog_api,
-    adapter: Ecto.Adapters.Postgres
-end
--- books_api/lib/books_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule BooksApi.Repo do
-  use Ecto.Repo,
-    otp_app: :books_api,
-    adapter: Ecto.Adapters.Postgres
-end
